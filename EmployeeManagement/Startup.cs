84116df1f854f785b92378fc1e2f7f631f089615
maharoz <mahruj66@gmail.com>
2019-03-08 11:21:55 +0600

    mvc setup

--- EmployeeManagement/Startup.cs
+++ EmployeeManagement/Startup.cs
@@ -16,25 +16,25 @@ namespace EmployeeManagement
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env,ILogger<Startup> logger)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
-            {
+            {              
                 app.UseDeveloperExceptionPage();
             }
 
-         
-           
-            app.UseFileServer();
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
             
 
             app.Run(async (context) =>
             {
-                throw new Exception("Some error processing the request");
-                await context.Response.WriteAsync("Hello world");
+                //throw new Exception("Some error processing the request");
+                await context.Response.WriteAsync("Hello world" );
                
             });
         }
