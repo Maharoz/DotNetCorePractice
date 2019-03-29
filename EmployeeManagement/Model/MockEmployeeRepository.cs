0cbb282abe5de0ec739fc6874e78dc666b13d54a
maharoz <mahruj66@gmail.com>
2019-03-29 17:19:33 +0600

    Conventional routing added to the project

--- EmployeeManagement/Model/MockEmployeeRepository.cs
+++ EmployeeManagement/Model/MockEmployeeRepository.cs
@@ -14,8 +14,8 @@ namespace EmployeeManagement.Model
             _employeeList = new List<Employee>()
             {
                 new Employee(){Id = 1 , Name ="Mary" , Department="HR",Email = "marry@gmail.com"},
-                new Employee(){Id = 1 , Name ="John" , Department="IT",Email = "john@gmail.com"},
-                new Employee(){Id = 1 , Name ="Sam" , Department="IT",Email = "sam@gmail.com"}
+                new Employee(){Id = 2 , Name ="John" , Department="IT",Email = "john@gmail.com"},
+                new Employee(){Id = 2 , Name ="Sam" , Department="IT",Email = "sam@gmail.com"}
             };
         }
 
