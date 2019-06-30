9adf8e5b93afbd37a06e5357c75e1d2fcfb71071
maharoz <mahruj66@gmail.com>
2019-06-30 21:32:26 +0600

    Error handling usestatuscodepageswithreexecute

--- EmployeeManagement/Controllers/ErrorController.cs
+++ EmployeeManagement/Controllers/ErrorController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Routing;
 
@@ -12,10 +13,13 @@ namespace EmployeeManagement.Controllers
         [Route("Error/{statusCode}")]
         public IActionResult HttpStatusCodeHandler(int statusCode)
         {
+            var statusCodeResult = HttpContext.Features.Get<IStatusCodeReExecuteFeature>();
             switch (statusCode)
             {
                 case 404:
                     ViewBag.ErrorMessage = "Sorry , the resources you requested Could not be found ";
+                    ViewBag.path = statusCodeResult.OriginalPath;
+                    ViewBag.qs = statusCodeResult.OriginalQueryString;
                     break;
             }
             return View("NotFound");
