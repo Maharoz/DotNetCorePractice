accecccd0da70a46f8b8f6c367ffb1386ca4996e
maharoz <mahruj66@gmail.com>
2019-08-17 21:08:02 +0600

    show hide login logout

--- EmployeeManagement/Controllers/AccountController.cs
+++ EmployeeManagement/Controllers/AccountController.cs
@@ -19,6 +19,15 @@ namespace EmployeeManagement.Controllers
             this.userManager = userManager;
             this.signInManager = signInManager;
         }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            await signInManager.SignOutAsync();
+            return RedirectToAction("index", "Home");
+        }
+
         // GET: /<controller>/
         [HttpGet]
         public IActionResult Register()
